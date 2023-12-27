87d7371b6f877cb2f9eccef4dc641367fa13df5d
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-12-27 19:24:58 +0900

    요구사항2: 응답 데이터를 처리하는 로직을 별도의 클래스로 분리한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,21 +3,15 @@ package webserver;
 import db.DataBase;
 import http.HttpMethod;
 import http.HttpRequest;
+import http.HttpResponse;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -33,9 +27,9 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            DataOutputStream dos = new DataOutputStream(out);
 
             HttpRequest request = new HttpRequest(in);
+            HttpResponse response = new HttpResponse(out);
 
             HttpMethod requestMethod = request.getMethod();
             String path = request.getPath();
@@ -43,30 +37,12 @@ public class RequestHandler extends Thread {
             log.debug("path = {}", path);
 
             if (path.equals("/index.html")) {
-                Path resourcePath = Paths.get("./webapp/index.html");
-                byte[] body = Files.readAllBytes(resourcePath);
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                response.forward("/index.html");
                 return;
             }
 
             if (path.equals("/user/form.html")) {
-                Path resourcePath = Paths.get("./webapp/user/form.html");
-                byte[] body = Files.readAllBytes(resourcePath);
-                response200Header(dos, body.length);
-                responseBody(dos, body);
-                return;
-            }
-
-            if (path.equals("/user/create") && requestMethod.isGetMethod()) {
-                User user = new User(
-                        request.getHeader("userId"),
-                        request.getHeader("password"),
-                        request.getHeader("name"),
-                        request.getHeader("email")
-                );
-
-                DataBase.addUser(user);
+                response.forward("/user/form.html");
                 return;
             }
 
@@ -79,16 +55,13 @@ public class RequestHandler extends Thread {
                 );
 
                 DataBase.addUser(user);
-                response302Header(dos, "/index.html");
+                response.sendRedirect("/index.html", null);
                 log.debug("user : {}, 저장 완료", user);
                 return;
             }
 
             if (path.equals("/user/login.html")) {
-                Path resourcePath = Paths.get("./webapp/user/login.html");
-                byte[] body = Files.readAllBytes(resourcePath);
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                response.forward("/user/login.html");
                 return;
             }
 
@@ -97,137 +70,37 @@ public class RequestHandler extends Thread {
 
                 // 로그인 실패시 login_failed.html로 리다이렉트
                 if (user == null) {
-                    response302HeaderWithLoginFailed(dos, "/user/login_failed.html");
+                    response.sendRedirect("/user/login_failed.html", Boolean.FALSE);
                     return;
                 }
 
                 if (!request.getParameter("password").equals(user.getPassword())) {
-                    response302HeaderWithLoginFailed(dos, "/user/login_failed.html");
+                    response.sendRedirect("/user/login_failed.html", Boolean.FALSE);
                     return;
                 }
 
                 // 로그인 성공시
                 log.debug("로그인 성공");
-                response302HeaderWithLoginSuccess(dos, "/index.html");
+                response.sendRedirect("/index.html", Boolean.TRUE);
                 return;
             }
 
             if (path.equals("/user/login_failed.html")) {
-                Path resourcePath = Paths.get("./webapp/user/login_failed.html");
-                byte[] body = Files.readAllBytes(resourcePath);
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                response.forward("/user/login_failed.html");
                 return;
             }
 
-            if (path.equals("/user/list")) {
+            if (path.equals("/user/list.html")) {
                 if (request.isLogin()) {
-                    Collection<User> users = DataBase.findAll();
-                    String body = getUserInfoBody(users);
-                    response200Header(dos, body.length());
-                    responseBody(dos, body.getBytes());
+                    response.forward("/user/list.html");
                     return;
                 }
 
-                response302Header(dos, "/user/login.html");
+                response.forward("/user/login.html");
                 return;
             }
 
-            if (path.equals("/css/styles.css")) {
-                Path resourcePath = Paths.get("./webapp/css/styles.css");
-                byte[] body = Files.readAllBytes(resourcePath);
-                response200CssHeader(dos, body.length);
-                responseBody(dos, body);
-                return;
-            }
-
-            if (path.equals("/css/bootstrap.min.css")) {
-                Path resourcePath = Paths.get("./webapp/css/bootstrap.min.css");
-                byte[] body = Files.readAllBytes(resourcePath);
-                response200CssHeader(dos, body.length);
-                responseBody(dos, body);
-                return;
-            }
-
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200CssHeader(DataOutputStream dos, int length) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + length + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private static String getUserInfoBody(Collection<User> users) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<table border='1'>");
-        for (User user : users) {
-            sb.append("<tr>");
-            sb.append("<td>" + user.getUserId() + "</td>");
-            sb.append("<td>" + user.getName() + "</td>");
-            sb.append("<td>" + user.getEmail() + "</td>");
-            sb.append("</tr>");
-        }
-        return sb.toString();
-    }
-
-    private void response302HeaderWithLoginFailed(DataOutputStream dos, String redirectUri) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found\r\n");
-            dos.writeBytes("Set-Cookie: logined=false; Path=/\r\n");
-            dos.writeBytes("Location: " + redirectUri + "\r\n"); // 리다이렉트할 URL을 여기에 넣으세요
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response302HeaderWithLoginSuccess(DataOutputStream dos, String redirectUri) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found\r\n");
-            dos.writeBytes("Set-Cookie: logined=true; Path=/\r\n");
-            dos.writeBytes("Location: " + redirectUri + "\r\n"); // 리다이렉트할 URL을 여기에 넣으세요
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String redirectUri) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found\r\n");
-            dos.writeBytes("Location: " + redirectUri + "\r\n"); // 리다이렉트할 URL을 여기에 넣으세요
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            response.forward(path);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
